789e71a35ff1bae9b38a664a3a05b75dda48e38c
kathrin <ge38zor@mytum.de>
2023-05-23 20:37:36 +0200

    add product component andchange the color of topnav bar

---
+++ src/views/components/product/index.js
@@ -0,0 +1,25 @@
+// 引入样式文件
+import './index.css';
+
+function Page() {
+  return (
+    // HTML结构
+    <div className="product">
+      {/* should link to the corresponding product detail page*/}
+        <a href="#">
+          <div className="product-test-img"></div>
+          <div className="product-name">
+            Hafer Porridge Cup Schokolade - Davert - 65 g
+          </div>
+        </a>
+        <div className="product-other-content">
+            <div className="product-nutri"></div>
+            <div className="product-right-buttom">
+              <div className="product-unitprice">2,99€</div>
+              <button/>
+            </div>
+        </div>
+    </div>
+  );
+}
+export default Page;
