07fac02d2ef52be4259f349c7fae57cc9148db9e
Yiheng Zhu <ge38rob@mytum.de>
2023-06-13 15:40:00 +0200

    homepage category jump to allProducts page “setCategory is not a function” bug fixed

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,21 +1,24 @@
 // 引入样式文件
-import './index.css';
+import "./index.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function Page({setCategory}) {
-  const [selectedCategory, setSelectedCategory] = useState(window.location.href.split("/")[3].split("#")[1]);
-  useEffect(() => {console.log("category component: " + selectedCategory);},[selectedCategory]);
-  const handleCategoryChange = (event)=>{
-    const {className} = event.target;
+function Page({ setCategory = () => undefined }) {
+  const [selectedCategory, setSelectedCategory] = useState(
+    window.location.href.split("/")[3].split("#")[1]
+  );
+  useEffect(() => {
+    console.log("category component: " + selectedCategory);
+  }, [selectedCategory]);
+  const handleCategoryChange = (event) => {
+    const { className } = event.target;
     const category = className.split(" ")[1].split("-")[0];
-    if (selectedCategory===category){
+    if (selectedCategory === category) {
       setCategory(() => undefined);
       setSelectedCategory(() => undefined);
-    }
-    else{
-      setCategory(() => category);
+    } else {
       setSelectedCategory(() => category);
+      setCategory(() => category);
     }
   };
   /*
@@ -32,37 +35,63 @@ function Page({setCategory}) {
     <div className="category-container">
       <div className="category">
         <div className="category-icon">
-          <Link to={(selectedCategory==="staple") ? "/product" : "/product#staple"}  
-          className={(selectedCategory==="staple") ? "category-icon staple-chosen" : "category-icon staple-non-chosen"} 
-          onClick={handleCategoryChange}/>
+          <Link
+            to={selectedCategory === "staple" ? "/product" : "/product#staple"}
+            className={
+              selectedCategory === "staple"
+                ? "category-icon staple-chosen"
+                : "category-icon staple-non-chosen"
+            }
+            onClick={handleCategoryChange}
+          />
         </div>
         <p>Staple</p>
       </div>
       <div className="category">
         <div className="category-icon">
-          <Link to={(selectedCategory==="snacks") ? "/product" : "/product#snacks"}  
-          className={(selectedCategory==="snacks") ? "category-icon snacks-chosen" : "category-icon snacks-non-chosen"} 
-          onClick={handleCategoryChange}/>
+          <Link
+            to={selectedCategory === "snacks" ? "/product" : "/product#snacks"}
+            className={
+              selectedCategory === "snacks"
+                ? "category-icon snacks-chosen"
+                : "category-icon snacks-non-chosen"
+            }
+            onClick={handleCategoryChange}
+          />
         </div>
         <p>Snacks</p>
       </div>
       <div className="category">
         <div className="category-icon">
-          <Link to={(selectedCategory==="drinks") ? "/product" : "/product#drinks"}  
-          className={(selectedCategory==="drinks") ? "category-icon drinks-chosen" : "category-icon drinks-non-chosen"} 
-          onClick={handleCategoryChange}/>
+          <Link
+            to={selectedCategory === "drinks" ? "/product" : "/product#drinks"}
+            className={
+              selectedCategory === "drinks"
+                ? "category-icon drinks-chosen"
+                : "category-icon drinks-non-chosen"
+            }
+            onClick={handleCategoryChange}
+          />
         </div>
         <p>Drinks</p>
       </div>
       <div className="category">
         <div className="category-icon">
-          <Link to="#" className="category-icon snacks-non-chosen" onClick={handleCategoryChange}/>
+          <Link
+            to="#"
+            className="category-icon snacks-non-chosen"
+            onClick={handleCategoryChange}
+          />
         </div>
         <p>Xxxx</p>
       </div>
       <div className="category">
         <div className="category-icon">
-          <Link to="#" className="category-icon snacks-non-chosen" onClick={handleCategoryChange}/>
+          <Link
+            to="#"
+            className="category-icon snacks-non-chosen"
+            onClick={handleCategoryChange}
+          />
         </div>
         <p>Xxxx</p>
       </div>
