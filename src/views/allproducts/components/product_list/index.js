43e34a1e16bb0e6699596587d59bd1640aa2fe47
Yiheng Zhu <ge38rob@mytum.de>
2023-06-19 01:16:30 +0200

    distinguish page loading and no results found

--- src/views/allproducts/components/product_list/index.js
+++ src/views/allproducts/components/product_list/index.js
@@ -1,9 +1,21 @@
 import "./index.css";
 import Product from "../../../../components/product";
-
+import { useState, useEffect } from "react";
 function ProductList({ productlist = [] }) {
   /* no corresponding product which may occurs when apply filters or input sth. wrong */
-  if (productlist.length === 0) {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="products-container">
+        <h1>Loading...</h1>
+      </div>
+    );
+  } else if (productlist.length === 0) {
     return (
       <div className="products-container">
         <h1>Sorry, there is no product...</h1>
