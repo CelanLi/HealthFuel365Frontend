26f08b4310c0516a1c95aceabee051d7392bfb98
kathrin <ge38zor@mytum.de>
2023-06-19 14:26:36 +0200

    refine pagination display and alternatives

--- src/views/allproducts/components/product_list/index.js
+++ src/views/allproducts/components/product_list/index.js
@@ -1,24 +1,21 @@
 import "./index.css";
 import Product from "../../../../components/product";
-import { useState, useEffect } from "react";
-function ProductList({ productlist = [] }) {
-  /* no corresponding product which may occurs when apply filters or input sth. wrong */
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  }, []);
+
+function ProductList({ productlist = [], isLoading }) {
   if (isLoading) {
     return (
       <div className="products-container">
-        <h1>Loading...</h1>
+        <div className="center">
+          <h1>Loading...</h1>
+        </div>
       </div>
     );
-  } else if (productlist.length === 0) {
+  } else if (productlist.length === 0) { // no corresponding product which may occurs when apply filters or input sth. wrong 
     return (
       <div className="products-container">
-        <h1>Sorry, there is no product...</h1>
+        <div className="center">
+          <h1>Sorry, there is no product...</h1>
+        </div>
       </div>
     );
   } else
