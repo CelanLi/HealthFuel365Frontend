b87df0c57c7ebc62f6cf70b5b1870dc9f709a452
Yiheng Zhu <ge38rob@mytum.de>
2023-06-24 23:42:49 +0200

    After clicking on the add to shoppingcart button, users will be redirected to the welcome page

--- src/views/productdetail/index.js
+++ src/views/productdetail/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { message } from "antd";
+import { message, Modal } from "antd";
 import Nutri from "../../components/nutri";
 import ContentLevel from "./components/content_level";
 import AddToScButton from "../../components/add_to_sc_button";
@@ -32,248 +32,32 @@ function Page() {
     };
     setData(id);
     (async () => {
-      const cookie = getCookie("login")
+      const cookie = getCookie("login");
       if (cookie) {
         const userAccount = await getUser();
         const userID = userAccount.id;
         setShoppingCartID(userID);
-      }
-      else{
+      } else {
         messageApi.open({
           type: "error",
           content: "Please log in!",
         });
       }
-      
-    })()
+    })();
   }, []);
-  // {
-  //   productID: "p1",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "C",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-  // {
-  //   productID: "p2",
-  //   productName: "Couscous Vollkorn - Davert - 500g",
-  //   productPrice: 12.99,
-  //   productNutri: "A",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/930/6109/front_de.6.full.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 5,
-  // },
-  // {
-  //   productID: "p3",
-  //   productName: "Saltoos - Seitenbacher - 200 g",
-  //   productPrice: 4.84,
-  //   productNutri: "A",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/200/000/009/8459/front_de.3.400.jpg",
-  //   productBrand: "Seitenbacher",
-  //   capacity: 5,
-  // },
-  // {
-  //   productID: "p4",
-  //   productName: "Seitenbacher all natural cereal musli strawberry delight",
-  //   productPrice: 12.99,
-  //   productNutri: "A",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/003/954/509/9095/front_en.3.400.jpg",
-  //   productBrand: "Seitenbacher",
-  //   capacity: 20,
-  // },
-  // {
-  //   productID: "p5",
-  //   productName: "Milk & Cereal Biscuits - Belvita",
-  //   productPrice: 7.99,
-  //   productNutri: "B",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/762/221/074/0519/front_en.21.400.jpg",
-  //   productBrand: "Belvita",
-  //   capacity: 15,
-  // },
-  // {
-  //   productID: "p6",
-  //   productName: "Biscuits Belvita Choco - 300g",
-  //   productPrice: 3.99,
-  //   productNutri: "D",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/762/230/044/3269/front_en.19.400.jpg",
-  //   productBrand: "Belvita",
-  //   capacity: 1,
-  // },
-  // {
-  //   productID: "p7",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "A",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-  // {
-  //   productID: "p8",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "B",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-  // {
-  //   productID: "p9",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "C",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-  // {
-  //   productID: "p10",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "D",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-  // {
-  //   productID: "p11",
-  //   productName: "Hafer Porridge Cup Schokolade - Davert - 65 g",
-  //   productPrice: 2.99,
-  //   productNutri: "E",
-  //   imageUrl:
-  //     "https://images.openfoodfacts.org/images/products/401/933/963/6107/front_de.22.400.jpg",
-  //   productBrand: "Davert",
-  //   capacity: 3,
-  // },
-
-  // const [productDetailList, setProductDetail] = useState([
-  //   {
-  //     productID: "1",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "2",
-  //     fatLevel: "small",
-  //     fat: 1.8,
-  //     sugarLevel: "small",
-  //     sugar: 2.2,
-  //     saltLevel: "small",
-  //     salt: 0.01,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p3",
-  //     fatLevel: "small",
-  //     fat: 2,
-  //     sugarLevel: "small",
-  //     sugar: 4,
-  //     saltLevel: "small",
-  //     salt: 0,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p4",
-  //     fatLevel: "moderate",
-  //     fat: 15.6,
-  //     sugarLevel: "small",
-  //     sugar: 4.44,
-  //     saltLevel: "small",
-  //     salt: 0,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p5",
-  //     fatLevel: "small",
-  //     fat: 1.2,
-  //     sugarLevel: "moderate",
-  //     sugar: 6.1,
-  //     saltLevel: "moderate",
-  //     salt: 0.59,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p6",
-  //     fatLevel: "moderate",
-  //     fat: 14,
-  //     sugarLevel: "high",
-  //     sugar: 26,
-  //     saltLevel: "moderate",
-  //     salt: 0.51,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p7",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p8",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p9",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p10",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  //   {
-  //     productID: "p11",
-  //     fatLevel: "high",
-  //     fat: 28,
-  //     sugarLevel: "moderate",
-  //     sugar: 12,
-  //     saltLevel: "small",
-  //     salt: 0.06,
-  //     productDescription: ["D1: .....", "D2: ....."],
-  //   },
-  // ]);
 
   var isAvailable = (capacity) => {
     if (capacity > 0) return "Available";
     else return "Not Available";
+  };
+  const showLoginReminder = () => {
+    Modal.error({
+      title: "please log in",
+      content: "Before adding items to your shopping cart, log in is reuired",
+      onOk: () => {
+        window.location.href = "http://localhost:3000";
+      },
+    });
   };
 
   const clickShoppingCart = async (id) => {
@@ -285,11 +69,10 @@ function Page() {
     if (product?.capacity === 0) {
       message.error("Sorry, this item is not available.");
     }
-    const cookie = getCookie("login")
+    const cookie = getCookie("login");
     if (!cookie) {
-      message.error("Please log in!");
-    }
-    else {
+      showLoginReminder();
+    } else {
       clickShoppingCart(id);
     }
   };
