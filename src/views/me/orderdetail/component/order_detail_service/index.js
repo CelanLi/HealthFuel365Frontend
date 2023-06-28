2b7a0d3ad86316ef22e04dae9f74d305024b9ccf
Chen <celan.li@tum.de>
2023-06-28 15:19:17 +0200

    package & shipping services refine

--- src/views/me/orderdetail/component/order_detail_service/index.js
+++ src/views/me/orderdetail/component/order_detail_service/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import './index.css'
 
+import GiftIcon from '../../../../../assets/images/gift-icon.png'
+import DeliveryIcon from '../../../../../assets/images/delivery-icon.png'
+import FastDeliveryIcon from '../../../../../assets/images/box-icon.png'
 import { getServiceByOrderId } from '../../../../../services/orderService';
 
 function OrderServices({orderID}) {
@@ -31,27 +34,42 @@ function OrderServices({orderID}) {
     <div className='order-service-wrap'>
       <div>
         {services.isDHL ? (
-          <div className='order-service-row'>
-            <div className='myaccount-order-text'>Standard shipping via DHL</div>
-            <div className='myaccount-order-text'>4.95€</div>
+          <div className='order-service-block'>
+            <img src = {DeliveryIcon} className="myaccount-address-icon"/>
+            <div className='order-service-row'>
+              <div className='myaccount-order-text'>Standard shipping via DHL</div>
+              <div className='myaccount-order-text'>4.95€</div>
+            </div>
           </div>
+          
         ) : services.rapidShipping ? (
-          <div className='order-service-row'>
-            <div className='myaccount-order-text'>Rapid packaging and shipping (1-2 Working days)</div>
-            <div className='myaccount-order-text'>7.95€</div>
+          <div className='order-service-block'>
+            <img src = {FastDeliveryIcon} className="myaccount-address-icon"/>
+            <div className='order-service-row'>
+              
+              <div className='myaccount-order-text'>Rapid packaging and shipping (1-2 Working days)</div>
+              <div className='myaccount-order-text'>7.95€</div>
+            </div>
           </div>
         ) : (
-          <div className='order-service-row'>
-            <div className='myaccount-order-text'>Standard shipping via HERMES</div>
-            <div className='myaccount-order-text'>7.95€</div>
+          <div className='order-service-block'>
+            <img src = {DeliveryIcon} className="myaccount-address-icon"/>
+            <div className='order-service-row'>
+              
+              <div className='myaccount-order-text'>Standard shipping via HERMES</div>
+              <div className='myaccount-order-text'>7.95€</div>
+            </div>
           </div>
         )}
       </div>
       <div>
         {services.sendAsAGift && (
-          <div className='order-service-row'>
-            <div className='myaccount-order-text'>I want to send as a gift</div>
-            <div className='myaccount-order-text'>1.95€</div>
+          <div className='order-service-block'>
+            <img src = {GiftIcon} className="myaccount-address-icon"/>
+            <div className='order-service-row'>
+              <div className='myaccount-order-text'>I want to send as a gift</div>
+              <div className='myaccount-order-text'>1.95€</div>
+            </div>
           </div>
         )}
       </div>
