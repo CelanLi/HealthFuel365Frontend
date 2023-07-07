fb3930310effb5446922748f7b5452463f84ae27
Yiheng Zhu <ge38rob@mytum.de>
2023-07-08 00:46:34 +0200

    unknown nova icon added

--- src/views/productdetail/components/nova/index.js
+++ src/views/productdetail/components/nova/index.js
@@ -1,24 +1,23 @@
 import "./index.css";
 
-function NovaScore({nova}) {
+function NovaScore({ nova }) {
   function getNovaScore(nova = "1.0") {
-    return require(`../../../../assets/images/nova_score/novascore${nova}.png`);
+    return require(`../../../../assets/images/nova_score/novascore${nova}.jpeg`);
   }
   function getNovaText(nova = "1.0") {
-    if (nova === "1.0"){
+    if (nova === "1.0") {
       return "Unprocessed or minimally processed foods";
-    }
-    else if (nova === "2.0"){
+    } else if (nova === "2.0") {
       return "Processed culinary ingredients";
-    }
-    else if (nova === "3.0"){
+    } else if (nova === "3.0") {
       return "Processed foods";
-    }
-    else return "Ultra processed foods";
+    } else if (nova === "4.0") {
+      return "Ultra processed foods";
+    } else return "Unknown Nova";
   }
   return (
     <div className="nova">
-      <img src={getNovaScore(nova)} className="nova-img"/>
+      <img src={getNovaScore(nova)} className="nova-img" />
       <p> {getNovaText(nova)} </p>
     </div>
   );
