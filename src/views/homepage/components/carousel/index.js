655183e825b7f8d074f80ac4e429be4c500f446f
kathrin <ge38zor@mytum.de>
2023-07-14 12:57:33 +0200

    refine carousel

--- src/views/homepage/components/carousel/index.js
+++ src/views/homepage/components/carousel/index.js
@@ -1,10 +1,11 @@
 import { Carousel } from 'antd';
-
+import "./index.css"
 
 // size of carousel images: 1450*500
-import Carousel1 from '../../../../assets/images/homepage/Healthy-Snacks.jpg'
-import Carousel2 from '../../../../assets/images/homepage/snacks2.jpg'
+import Carousel1 from '../../../../assets/images/homepage/promocode.jpeg'
+import Carousel2 from '../../../../assets/images/homepage/Healthy-Snacks.jpg'
 
+/*
 const contentStyle = {
   height: '200px',
   color: '#fff',
@@ -12,21 +13,24 @@ const contentStyle = {
   textAlign: 'center',
   background: '#364d79',
 };
+*/
 
 const App = () => (
   <Carousel autoplay>
-    <div>
+    <div className="carousel-img">
         <img  src={Carousel1}/>
     </div>
-    <div>
+    <div className="carousel-img">
         <img  src={Carousel2}/>
     </div>
+    {/*
     <div>
       <h3 style={contentStyle}>3</h3>
     </div>
     <div>
       <h3 style={contentStyle}>4</h3>
     </div>
+    */}
   </Carousel>
 );
 export default App;
