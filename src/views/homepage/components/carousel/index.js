16ea998b7e68c276d78b92f3991aa344edc1fc46
Yiheng Zhu <ge38rob@mytum.de>
2023-07-16 18:06:23 +0200

    carousel images upload

--- src/views/homepage/components/carousel/index.js
+++ src/views/homepage/components/carousel/index.js
@@ -1,36 +1,21 @@
-import { Carousel } from 'antd';
-import "./index.css"
+import { Carousel } from "antd";
+import "./index.css";
 
-// size of carousel images: 1450*500
-import Carousel1 from '../../../../assets/images/homepage/promocode.jpeg'
-import Carousel2 from '../../../../assets/images/homepage/Healthy-Snacks.jpg'
-
-/*
-const contentStyle = {
-  height: '200px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
-*/
+import Carousel1 from "../../../../assets/images/homepage/promocode.jpeg";
+import Carousel2 from "../../../../assets/images/homepage/alnatura.jpg";
+import Carousel3 from "../../../../assets/images/homepage/gym.jpg";
 
 const App = () => (
   <Carousel autoplay>
     <div className="carousel-img">
-        <img  src={Carousel1}/>
+      <img src={Carousel1} />
     </div>
     <div className="carousel-img">
-        <img  src={Carousel2}/>
+      <img src={Carousel2} />
     </div>
-    {/*
-    <div>
-      <h3 style={contentStyle}>3</h3>
+    <div className="carousel-img">
+      <img src={Carousel3} />
     </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-    */}
   </Carousel>
 );
 export default App;
